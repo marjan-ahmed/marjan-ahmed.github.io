584ac89660832b20d250eaeaa696d9264ceeadfd
Marjan Ahmed <marjan.ahmed08@yahoo.com>
2026-01-24 01:11:50 +0500

    Add dynamic export for static rendering of Open Graph images

--- src/app/blog/[slug]/opengraph-image.tsx
+++ src/app/blog/[slug]/opengraph-image.tsx
@@ -4,6 +4,8 @@ import { ImageResponse } from "next/og";
 import { allPosts } from "content-collections";
 import { DATA } from "@/data/resume";
 
+export const dynamic = "force-static";
+
 // Required for static export
 export async function generateStaticParams() {
   return allPosts.map((post) => ({
